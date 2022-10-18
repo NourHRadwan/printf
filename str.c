ef31e47b86ff0c1ed5f63127c50f328f41e5124e
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-18 11:23:00 -0700

    print string

---
+++ str.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * str - Handle string.
+ *
+ * @list: passed args.
+ *
+ * Return: character count.
+ */
+
+int str(va_list list)
+{
+	int func_num = 0;
+	char *str;
+
+	str = va_arg(list, char *);
+	if (!str)
+		str = "(null)";
+
+	if (str[0] == '\0')
+		return (-1);
+
+	while (*str)
+		func_num += _putchar(*str++);
+
+	return (func_num);
+}
