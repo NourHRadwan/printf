d7447d9945458583d37256fec92a288fabf4c422
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-18 11:19:03 -0700

    char function

---
+++ chr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * chr - print char
+ *
+ * @list: list of arguments .
+ *
+ * Return: count of chars.
+ */
+
+int chr(va_list list)
+{
+	char c = va_arg(list, int);
+
+	return (write(1, &c, 1));
+
+}
